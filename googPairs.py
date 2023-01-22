489aae448a7e55cc1e8b9c0e1f3db4509f77a900
Leulseged Bekele <88849889+Leulbkm@users.noreply.github.com>
2023-01-22 14:09:18 +0300

    number of good pairs

--- googPairs.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def numIdenticalPairs(self, nums: List[int]) -> int:
-        counting = 0
-        for i in range(len(nums)):
-            for j in range(i, len(nums)):
-                if i < j and nums[i] == nums[j]:
-                    counting += 1
-        return counting
